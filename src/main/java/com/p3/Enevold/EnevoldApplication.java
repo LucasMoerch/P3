3e586bf43fb0af1dbfa5d333ab6ac05d864a7d09
Lucas Mørch <lucasmoerch04@icloud.com>
2025-09-17 12:50:37 +0200

    first commit

---
+++ src/main/java/com/p3/Enevold/EnevoldApplication.java
@@ -0,0 +1,13 @@
+package com.p3.Enevold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnevoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnevoldApplication.class, args);
+	}
+
+}
