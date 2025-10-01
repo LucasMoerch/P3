15f767d34a7bb90d4a380213cc0b55c2cef10a07
Mads Scriver <madsulrichscriver@hotmail.com>
2025-10-01 09:41:04 +0200

    I created a component that creates a button, the button loads a card with some of the neccesary buttons to track time

--- frontend/src/pages/home.ts
+++ frontend/src/pages/home.ts
@@ -1,12 +1,19 @@
 import { renderHealthCheck} from "../components/healthCheck";
+import { renderTimeTracker } from "../components/timeTracker/timeTracker";
 
 export function renderHomePage(): HTMLElement{
     const div = document.createElement("div");
     div.innerHTML = `<h1>Home page<h1>`;
 
+   
+
     const container = document.createElement("container");
     container.appendChild(div);
     container.appendChild(renderHealthCheck())
+
+     const timeTracking = document.createElement("div");
+    container.appendChild(timeTracking);
+    container.appendChild(renderTimeTracker())
 
     const loginLink = document.createElement("a");
     loginLink.innerHTML = "Login"
