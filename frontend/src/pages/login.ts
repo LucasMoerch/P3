0e8cb5e96d2a1ff7f7c4657f49177ebb5e7e303e
Oscar <oscar@badensoe.dk>
2025-10-02 16:08:03 +0200

    temperary

--- frontend/src/pages/login.ts
+++ frontend/src/pages/login.ts
@@ -1,9 +1,23 @@
+import {renderHealthCheck} from "../components/healthCheck";
+import {renderLogIn} from "../components/logInBtn";
+
 export function renderLoginPage(): HTMLElement{
     const div = document.createElement("div");
-    div.innerHTML = `<h1>Login page<h1>`;
+    div.innerHTML = `<h1>Log In page</h1>`;
 
-    const container = document.createElement("container");
+    div.className = "primary"
+    const container = document.createElement("div");
     container.appendChild(div);
+
+    const loginLink = document.createElement("a");
+    loginLink.innerHTML = "Log In"
+    loginLink.href = "/home"
+    loginLink.setAttribute("data-link", "");
+
+    container.appendChild(loginLink)
+    container.appendChild(renderLogIn())
+
+
 
     return container
 }
