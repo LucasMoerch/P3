b784afb5213edfeb7efd3755fc953f2d100ef9bc
Lucas Mørch <lucasmoerch04@icloud.com>
2025-10-02 14:46:12 +0200

    install prettier

--- frontend/src/routers/router.ts
+++ frontend/src/routers/router.ts
@@ -1,28 +1,28 @@
-import { renderHomePage } from "../pages/home";
-import { renderLoginPage } from "../pages/login"
-import {renderCasesPage} from "../pages/cases";
-import {renderClientPage} from "../pages/client";
-import {renderMyProfilePage} from "../pages/myProfile";
-import {renderStaffPage} from "../pages/staff";
+import { renderHomePage } from '../pages/home';
+import { renderLoginPage } from '../pages/login';
+import { renderCasesPage } from '../pages/cases';
+import { renderClientPage } from '../pages/client';
+import { renderMyProfilePage } from '../pages/myProfile';
+import { renderStaffPage } from '../pages/staff';
 
 export function resolveRoute(path: string): HTMLElement {
-    switch (path) {
-        case "/login":
-            return renderLoginPage()
+  switch (path) {
+    case '/login':
+      return renderLoginPage();
 
-        case "/cases":
-            return renderCasesPage();
+    case '/cases':
+      return renderCasesPage();
 
-        case "/client":
-            return renderClientPage();
+    case '/client':
+      return renderClientPage();
 
-        case "/myProfile":
-            return renderMyProfilePage();
+    case '/myProfile':
+      return renderMyProfilePage();
 
-        case "/staff":
-            return renderStaffPage();
+    case '/staff':
+      return renderStaffPage();
 
-        default: return renderHomePage();
-    }
-
+    default:
+      return renderHomePage();
+  }
 }
