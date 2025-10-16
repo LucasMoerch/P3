30aa1a274e2c71fe280af66da9b88a3ff6de1ad2
Salem <Salemzad@gmail.com>
2025-10-16 15:04:43 +0200

    Add Client API with add, fetch, update, delete endpoints

--- backend/src/main/java/com/p3/Enevold/clients/Client.java
+++ backend/src/main/java/com/p3/Enevold/clients/Client.java
@@ -1,4 +1,32 @@
 package com.p3.Enevold.clients;
 
-public class client {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document ("clients")
+public class Client {
+    @Id
+     private String id;
+     private String name;
+     private String contactEmail;
+     private String contactPhone;
+     private String notes;
+     private String createdAt;
+
+    // Getters
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getContactEmail() { return contactEmail; }
+    public String getContactPhone() { return contactPhone; }
+    public String getNotes() { return notes; }
+    public String getCreatedAt() { return createdAt; }
+
+    public void setId(String id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
+    public void setContactPhone(String contactPhone) { this.contactPhone = contactPhone; }
+    public void setNotes(String notes) { this.notes = notes; }
+    public void setCreatedAt(String createdAt) { this.createdAt = createdAt; }
+
+
 }
--- backend/src/main/java/com/p3/Enevold/clients/ClientRepository.java
+++ backend/src/main/java/com/p3/Enevold/clients/ClientRepository.java
@@ -1,4 +1,6 @@
 package com.p3.Enevold.clients;
 
-public interface ClientRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ClientRepository extends MongoRepository <Client, String>{
 }
