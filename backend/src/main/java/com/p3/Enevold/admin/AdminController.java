23a6de15811f41b89bece0c357ca5bc3d70e91ab
IceKT <navin004.s.w@gmail.com>
2025-10-21 10:11:37 +0200

    fixed an error

--- backend/src/main/java/com/p3/Enevold/admin/AdminController.java
+++ backend/src/main/java/com/p3/Enevold/admin/AdminController.java
@@ -5,6 +5,7 @@ import com.p3.Enevold.users.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
