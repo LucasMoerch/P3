cd2692d39cd7a99ff3db06d51da25f21350c5088
Rindochi <mohtashim240916@gmail.com>
2025-11-02 15:26:49 +0100

    Can now edit, does not save in the backend

--- frontend/src/components/cardComponent/cardComponent.ts
+++ frontend/src/components/cardComponent/cardComponent.ts
@@ -20,6 +20,43 @@ export function renderCard(edit?: Boolean): HTMLElement {
       'btn edit-button border-0 bg-transparent text-primary position-absolute top-0 end-0 m-3 fs-2';
     editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
     card.appendChild(editBtn);
+
+    editBtn.addEventListener('click', () => {
+        //the infoRows line collects all elements with the class .info-row, so make sure to name it this.
+        const infoRows = card.querySelectorAll('.info-row');
+        //Loops to make sure we get every .info-row class and gets the displayed value.
+        infoRows.forEach((row) => {
+            const valueSpan = row.querySelector('.value');
+            if (!valueSpan) return;
+
+              //Here it creates an input box, so the user can edit the value.
+            if (!valueSpan.querySelector('input')) {
+                const currentValue = valueSpan.textContent?.trim() || '';
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.value = currentValue; //Makes sure to insert the current value, after you click edit.
+                input.className = 'form-control text-end fw-semibold';
+                valueSpan.textContent = '';
+                valueSpan.appendChild(input);
+              }
+        });
+
+        //Save button implementation
+        const saveBtn = document.createElement('button');
+        saveBtn.className = 'btn btn-primary btn-lg';
+        saveBtn.innerText = 'Save';
+
+        const btnContainer = document.createElement('div');
+        btnContainer.className = 'd-flex justify-content-center mt-3';
+        btnContainer.appendChild(saveBtn);
+        card.appendChild(btnContainer);
+
+        saveBtn.addEventListener('click', () => {
+            overlay.remove();
+        });
+
+        editBtn.disabled = true; // Disable while editing, so you cant press it again.
+    });
   }
   // Append card inside overlay
 
